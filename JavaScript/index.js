d1c5b42c00e68000466c7b3daf4f9912d9028414
Aayush Bhattarai <bhattaraiaayush.78@gmail.com>
2023-09-15 22:40:38 +0300

    UI designed done! Javascript started...

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,26 @@
-console.log("Hello there")
+let result = {
+    "tag": "",
+    "free": false,
+    "role": false,
+    "user": "iamaayush",
+    "email": "iamaayush@edu.xamk.fi",
+    "score": 0.64,
+    "state": "undeliverable",
+    "domain": "edu.xamk.fi",
+    "reason": "invalid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": false,
+    "did_you_mean": "",
+    "format_valid": true
+
+}
+
+let str = ``
+for (key of Object.keys(result)){
+    str = str + `<div>${key}:${result[key]}</div>`
+
+}
+console.log(str)
+resultcontainer.innerHTML = str
